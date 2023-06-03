1c1c37790d87210fc9431901aa05815a091626bc
SALIK AHMED <92015547+ahmedsalik786@users.noreply.github.com>
2023-06-03 15:12:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let inputElement=document.getElementsByClassName("code");
+        for(let i=0;i<inputElement.length;i++){
+            inputElement[i].addEventListener("keyup",(event)=>{
+                let currElement=event.target;
+                let code=event.key.charCodeAt(0);
+                if(event.key==="Backspace"){
+                    let prevElement=currElement.prevElementSibling;
+                    if(prevElement){
+                        prevElement.focus();
+                    }
+                    return;
+                }
+                if(48<=code && code <=57){
+                    let nextElement=currElement.nextElementSibling;
+                    if(nextElement){
+                        nextElement.focus();
+                    }
+                }else{
+                    event.target.value="";
+                }
+            })
+        }
